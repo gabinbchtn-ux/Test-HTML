9df59ba34f36f8bb9d8528485fe8172faf9208c7
gabinbchtn-ux <gabinbchtn@gmail.com>
2026-04-07 14:10:16 +0200

    Modify Firebase config and auth codes
    Updated Firebase configuration and authentication codes.

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const firebaseConfig = {
     apiKey: "AIzaSyCBOP15Omhn96aRNVsOm-InfcM7YscAkjE",
     authDomain: "test-c59b4.firebaseapp.com",
     databaseURL: "https://test-c59b4-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "test-c59b4",
+    projectId: "test-c59b5",
     storageBucket: "test-c59b4.firebasestorage.app",
     messagingSenderId: "249535021863",
-    appId: "1:249535021863:web:87b75f582080b3ee82df02"
+    appId: "1:249535021863:web:43b75f582080b3ee82df02"
 };
 
 firebase.initializeApp(firebaseConfig);
